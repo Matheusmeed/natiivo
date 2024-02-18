aeb29691e0d6f50eb56c578a11d2d9466147d81d
Matheus <mms.medeiros12@gmail.com>
2024-02-18 17:16:07 -0300

    feat: creating and styling login form

--- src/pages/Login/components/LoginCard/index.tsx
+++ src/pages/Login/components/LoginCard/index.tsx
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import {
+  BrokerDiv,
   Card,
   GradientOverlay,
+  GuestDiv,
   Image,
   LeftSide,
   LogoDiv,
+  PasswordDiv,
   RightSide,
+  TitleDiv,
 } from './styles';
+import { FaRegEyeSlash, FaRegEye } from 'react-icons/fa6';
 
 const LoginCard = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
   return (
     <Card>
       <LeftSide>
@@ -17,7 +29,38 @@ const LoginCard = () => {
           <img src='/images/logo/natiivoLogo.png' alt='natiivoLogo' />
         </LogoDiv>
       </LeftSide>
-      <RightSide></RightSide>
+      <RightSide>
+        <TitleDiv>
+          <h2>ACCESS THE PLATFORM</h2>
+          <p>
+            Access as a spectator by clicking let me in or log in as a broker.
+          </p>
+        </TitleDiv>
+        <GuestDiv>
+          <h3>JOIN AS GUEST</h3>
+          <button>LET ME IN</button>
+        </GuestDiv>
+        <BrokerDiv>
+          <h3>I’M A BROKER</h3>
+          <input type='text' name='password' placeholder='Type your email' />
+          <PasswordDiv>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              name='password'
+              placeholder='Type your password'
+            />
+            <button onClick={togglePasswordVisibility}>
+              {showPassword ? (
+                <FaRegEyeSlash color='#F6F6F6' size={20} />
+              ) : (
+                <FaRegEye color='#F6F6F6' size={19} />
+              )}
+            </button>
+          </PasswordDiv>
+          <button>LOGIN -</button>
+          <p>Don’t have an account? Create new account</p>
+        </BrokerDiv>
+      </RightSide>
     </Card>
   );
 };
--- src/pages/Login/components/LoginCard/styles.ts
+++ src/pages/Login/components/LoginCard/styles.ts
@@ -6,11 +6,11 @@ export const Card = styled.div`
   padding: 48px;
   border-radius: 16px;
   gap: 48px;
-  box-shadow: 0px 109.543212890625px 82.15740966796875px 0px #0000000d;
   box-shadow: 1px 1px 5px 1px #ffffff52 inset;
   background: #000000a3;
   display: flex;
   gap: 30px;
+  backdrop-filter: blur(5px);
 `;
 
 export const LeftSide = styled.div`
@@ -56,6 +56,141 @@ export const LogoDiv = styled.div`
 `;
 
 export const RightSide = styled.div`
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   width: 400px;
   height: 500px;
+  display: flex;
+  flex-direction: column;
+  color: white;
+  gap: 48px;
+`;
+
+export const TitleDiv = styled.div`
+  h2 {
+    font-size: 32px;
+    font-weight: 400;
+    line-height: 44px;
+    letter-spacing: 0.03em;
+    text-align: left;
+    color: #ffffff;
+  }
+
+  p {
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 22px;
+    letter-spacing: 0em;
+    text-align: left;
+    opacity: 0.7;
+  }
+`;
+
+export const GuestDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 26px;
+  padding-bottom: 26px;
+  border-bottom: 2px solid transparent;
+  border-image: linear-gradient(
+      90deg,
+      rgba(255, 255, 255, 0.32) 12.9%,
+      rgba(255, 255, 255, 0) 106.54%
+    )
+    1;
+
+  h3 {
+    width: fit-content;
+    padding-bottom: 4px;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 19px;
+    letter-spacing: 0px;
+    color: #f6f6f6;
+    border-bottom: 1px solid transparent;
+    border-image: linear-gradient(
+        90deg,
+        rgba(255, 255, 255, 0.32) 12.9%,
+        rgba(255, 255, 255, 0) 106.54%
+      )
+      1;
+  }
+
+  button {
+    width: 117px;
+    height: 48px;
+    padding: 0px, 16px, 0px, 16px;
+    border-radius: 1000px;
+    gap: 8px;
+    background: #ffffff14;
+    border: none;
+    box-shadow: 0px 4px 4px 0px #00000040;
+    box-shadow: 1px 1px 5px 1px #ffffff52 inset;
+    color: #f6f6f6;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 17px;
+    letter-spacing: 0.04em;
+    text-align: center;
+  }
+`;
+
+export const BrokerDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  h3 {
+    width: fit-content;
+    padding-bottom: 4px;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 19px;
+    letter-spacing: 0px;
+    color: #f6f6f6;
+    border-bottom: 1px solid transparent;
+    border-image: linear-gradient(
+        90deg,
+        rgba(255, 255, 255, 0.32) 12.9%,
+        rgba(255, 255, 255, 0) 106.54%
+      )
+      1;
+  }
+
+  input {
+    padding: 16px;
+    border-radius: 600px;
+    background: #ffffff0f;
+    border: none;
+    box-shadow: 1px 1px 5px 1px #ffffff52 inset;
+    outline: none;
+    color: #f6f6f6;
+
+    &::placeholder {
+      font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI',
+        Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+        sans-serif;
+      font-size: 14px;
+      font-weight: 400;
+      line-height: 19px;
+      letter-spacing: 0em;
+      text-align: left;
+      color: #f6f6f6a6;
+    }
+  }
+`;
+
+export const PasswordDiv = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+
+  button {
+    position: absolute;
+    right: 14px;
+    transform: translateY(50%);
+    background: none;
+    border: none;
+    cursor: pointer;
+  }
 `;
