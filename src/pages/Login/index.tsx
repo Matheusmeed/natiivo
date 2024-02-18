f8cdacd49336cf840e1de224bbce7ab3cb8e2d28
Matheus <mms.medeiros12@gmail.com>
2024-02-18 02:08:16 -0300

    feat: creating stand by

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,20 @@
-import { GradientWrapper, Wrapper } from './styles';
+import { ContentDiv, GradientWrapper, StartDiv, Wrapper } from './styles';
+import { HiArrowNarrowRight } from 'react-icons/hi';
 
 const LoginPage: React.FC = () => {
   return (
     <Wrapper>
-      <GradientWrapper></GradientWrapper>
+      <GradientWrapper>
+        <ContentDiv>
+          <img src='/images/logo/natiivoLogo.png' alt='natiivo logo' />
+          <StartDiv>
+            <h3>START EXPERIENCE</h3>
+            <button>
+              <HiArrowNarrowRight color='#FFFFFF' size={'100%'} />
+            </button>
+          </StartDiv>
+        </ContentDiv>
+      </GradientWrapper>
     </Wrapper>
   );
 };
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -13,5 +13,85 @@ export const Wrapper = styled.div`
 export const GradientWrapper = styled.div`
   height: 100vh;
   width: 100%;
-  background: radial-gradient(circle, #0000 0%, #00000017 50%, #000000d4 100%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(
+      90deg,
+      rgba(0, 0, 0, 0) 72.4%,
+      rgba(3, 10, 12, 0.64) 100%
+    ),
+    linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(0, 0, 0, 0) 83.85%,
+      rgba(3, 10, 12, 0.64) 100%
+    ),
+    linear-gradient(90deg, rgba(3, 10, 12, 0.64) 0%, rgba(0, 0, 0, 0) 25.7%),
+    linear-gradient(
+      180deg,
+      rgba(3, 10, 12, 0.64) 0%,
+      rgba(0, 0, 0, 0) 16.15%,
+      rgba(0, 0, 0, 0) 100%
+    );
+`;
+
+export const ContentDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 484px;
+    height: 262.96px;
+  }
+`;
+
+export const StartDiv = styled.div`
+  position: relative;
+  margin-top: 26px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 273px;
+  height: 100px;
+
+  h3 {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI',
+      Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+      sans-serif;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 33px;
+    letter-spacing: 4px;
+    text-align: center;
+    color: #f6f6f6;
+    margin: 0;
+  }
+
+  button {
+    position: absolute;
+    bottom: 10px; /* Ajuste a distância da parte inferior conforme necessário */
+    width: 38px;
+    height: 38px;
+    padding: 8px;
+    border-radius: 1000px;
+    gap: 35px;
+    background: #ffffff14;
+    border: none;
+    box-shadow: 1px 1px 5px 1px #ffffff52 inset;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: 0.2s all; /* Adicionando transição para todas as propriedades */
+
+    &:hover {
+      cursor: pointer;
+      bottom: 0; /* Ajuste conforme necessário para centralizar */
+      width: 48px;
+      height: 48px;
+      background-color: #ffffff2b;
+    }
+  }
 `;
