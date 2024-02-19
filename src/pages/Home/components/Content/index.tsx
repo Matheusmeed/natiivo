68a66f37283a6d9de535069657e316b4e5707ddd
Matheus <mms.medeiros12@gmail.com>
2024-02-19 01:52:18 -0300

    feat: home header

--- src/pages/Home/components/Content/index.tsx
+++ src/pages/Home/components/Content/index.tsx
@@ -1,7 +1,56 @@
-import { Wrapper } from './styles';
+import { useState } from 'react';
+import {
+  HeaderDiv,
+  LogoDiv,
+  NavigationDiv,
+  NavigationOption,
+  Wrapper,
+} from './styles';
 
+type OptionsType = 'HOME' | 'PROJECT' | 'SALES';
 const Content: React.FC = () => {
-  return <Wrapper></Wrapper>;
+  const [selectedOption, setSelectedOption] = useState<OptionsType>('HOME');
+
+  const changeSelectedOption = (newOption: OptionsType) => {
+    setSelectedOption(newOption);
+  };
+
+  return (
+    <Wrapper>
+      <HeaderDiv>
+        <NavigationDiv>
+          <NavigationOption
+            isSelected={selectedOption === 'HOME'}
+            onClick={() => {
+              changeSelectedOption('HOME');
+            }}
+          >
+            HOME
+          </NavigationOption>
+          <NavigationOption
+            isSelected={selectedOption === 'PROJECT'}
+            onClick={() => {
+              changeSelectedOption('PROJECT');
+            }}
+          >
+            PROJECT
+          </NavigationOption>
+          <NavigationOption
+            isSelected={selectedOption === 'SALES'}
+            onClick={() => {
+              changeSelectedOption('SALES');
+            }}
+          >
+            SALES
+          </NavigationOption>
+        </NavigationDiv>
+        <LogoDiv>
+          <img src='/images/util/profile.png' alt='profile' />
+          <img src='/images/util/natiivoText.png' alt='natiivo' />
+        </LogoDiv>
+      </HeaderDiv>
+    </Wrapper>
+  );
 };
 
 export default Content;
--- src/pages/Home/components/Content/styles.ts
+++ src/pages/Home/components/Content/styles.ts
@@ -3,4 +3,59 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const HeaderDiv = styled.div`
+  margin: 20px 30px;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const NavigationDiv = styled.div`
+  width: 100%;
+  margin-bottom: 15px;
+  display: flex;
+  gap: 18px;
+
+  border-bottom: 1px solid;
+  border-image: linear-gradient(
+      90deg,
+      rgba(255, 255, 255, 0.32) 63.62%,
+      rgba(255, 255, 255, 0) 106.54%
+    )
+    1;
+`;
+
+export const NavigationOption = styled.button<{ isSelected?: boolean }>`
+  width: fit-content;
+  border: none;
+  padding: 0;
+  background: none;
+  font-size: 16px;
+  line-height: 19px;
+  letter-spacing: 0.03em;
+  text-align: left;
+  cursor: pointer;
+  font-weight: ${({ isSelected }) => (isSelected ? 'bold' : '400')};
+  color: ${({ isSelected }) => (isSelected ? 'white' : '#dbdbdcc4')};
+  border-bottom: ${({ isSelected }) =>
+    isSelected ? '1px solid white' : 'none'};
+  margin-bottom: -0.84px;
+
+  &:hover {
+    color: white;
+    font-weight: bold;
+    transition: 0.3s;
+  }
+`;
+
+export const LogoDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+  margin-left: 40px;
 `;
